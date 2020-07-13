5bd8fd3f509e5412b2b08fbb1735085e3ca7430a
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2020-07-13 20:51:57 +0200

    Basic models

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,26 +1,17 @@
 import { Address } from './address.model'
-
-export interface HealthInsurance {
-  card_number: string;
-  name: string;
-  type: string;
-}
+import { Insurance } from './insurance.model'
 
 export interface User {
   id: number;
   nhc: string;
   name: string;
   first_surname: string;
-  second_surname: string;
-  gender: string;
-  birth_date: string;
-  id_document: string;
-  address: Address;
-  health_insurance: HealthInsurance[];
+  second_surname?: string;
+  gender?: string;
+  birth_date?: string;
+  id_document?: string;
+  address?: Address;
+  health_insurance?: Insurance[];
   colegiate_number: string;
-  type: string;
-}
-
-export interface RootObject {
-  users: User[];
+  profesional_type?: string;
 }
