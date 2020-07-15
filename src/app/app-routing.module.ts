36a54f5e86a977aa36bf52806da7cce2762f1e04
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2020-07-15 10:19:43 +0200

    Edit user view created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './views/user-list/user-list.component';
 import { UserDetailComponent } from './views/user-detail/user-detail.component';
 import { NewUserComponent } from './views/new-user/new-user.component';
+import { EditUserComponent } from './views/edit-user/edit-user.component';
+import { Error404Component } from './views/error404/error404.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'users', pathMatch: 'full'},
   { path: 'users', component: UserListComponent },
   { path: 'users/new', component:NewUserComponent },
-  { path: 'users/:user_id', component:UserDetailComponent }
+  { path: 'users/:user_id', component:UserDetailComponent },
+  { path: 'users/:user_id/edit', component:EditUserComponent }
 ];
 
 @NgModule({
